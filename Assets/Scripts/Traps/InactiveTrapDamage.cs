d64df11bc9f612479d3ed4e9f264f167fb8f1403
Serhii <94123586+StpdSerega@users.noreply.github.com>
2023-10-25 01:40:15 +0300

    Add HP, Damage, Traps

---
+++ Assets/Scripts/Traps/InactiveTrapDamage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    public int damage = 1; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+    }
+}
